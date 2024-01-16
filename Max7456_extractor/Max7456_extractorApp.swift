7fe61560bde43ff7d3cca37e025f87a275687a12
Roman Andrykevych <romiroma@icloud.com>
2024-01-16 19:20:20 +0200

    Initial Commit

---
+++ Max7456_extractor/Max7456_extractorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Max7456_extractorApp.swift
+//  Max7456_extractor
+//
+//  Created by Roman on 16.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Max7456_extractorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
